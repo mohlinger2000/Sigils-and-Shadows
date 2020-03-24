ae8ee21b56e4d3e3abdbd393b66f83ad159e64cd
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2020-03-23 21:12:40 -0400

    Skeleton creation of branch to guide future dev

--- main.lua
+++ main.lua
@@ -1,13 +1,23 @@
+platform = require 'platform'
+
 function love.load()
 
 end
 
-function love.update(dt)
+
+function love.keypressed(key)
+    if key == 'escape' then
+        -- function LOVE gives us to terminate application
+        love.event.quit()
+    end
+end
+
+function love.update(dt) -- update used for changing all the variables for rendering
 
 end
 
 
-function love.draw()
+function love.draw() -- used for rendering of assets
 
 end
 
---
+++ platform.lua
@@ -0,0 +1,17 @@
+--[[----------------------------------------|
+Author: Vineeth                             |
+Date: 3/23/2020                             |
+Purpose: The purpose of this file is to     |
+act as a plugin library that can be used to |
+quickly make a platformer                   |
+]]------------------------------------------|
+
+
+
+-- Protaganist
+
+
+-- Terrain
+
+
+-- Pits
