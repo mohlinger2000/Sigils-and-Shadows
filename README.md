549ecc85485159c5e84d2b1c1750d9857f739fc5
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2020-07-20 22:34:58 -0400

    Basic Boilerplate for Breezefield

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Sigils and Shadows
 Platformer written in Lua to be run in LOVE2D game engine.
+
+
+## Libraries
+
+[Breezfield](https://github.com/HDictus/breezefield)
--- main.lua
+++ main.lua
@@ -1,14 +1,19 @@
-function love.load()
+-- Entrypoint file for game
+bf = require("breezefield")
 
+function love.load()
+    world = bf.newWorld(0,90.81,true)
+    ground = bf.Collider.new(world, "Polygon", {0, 550, 650, 550, 650, 0, 650})
+    ground:setType("static") 
 end
 
 function love.update(dt)
-
+    world:update(dt)
 end
 
 
 function love.draw()
-
+    world:draw()
 end
 
 
